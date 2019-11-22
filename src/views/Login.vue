816e6fa0964ddf619f1af42dd678df08563a730d
landeaux <pluckedowl@gmail.com>
2019-11-21 23:04:42 -0800

    add login view for login route

---
+++ src/views/Login.vue
@@ -0,0 +1,77 @@
+<script>
+import { mapState } from 'vuex'
+import { LOGIN } from '@/store/actions.type'
+
+export default {
+  name: 'RwvLogin',
+  data () {
+    return {
+      email: null,
+      password: null
+    }
+  },
+  computed: {
+    ...mapState({
+      errors: state => state.auth.errors
+    })
+  },
+  methods: {
+    onSubmit (email, password) {
+      this.$store
+        .dispatch(LOGIN, { email, password })
+        .then(() => this.$router.push({ name: 'home' }))
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">
+            Sign in
+          </h1>
+          <p class="text-xs-center">
+            <router-link :to="{ name: 'register' }">
+              Need an account?
+            </router-link>
+          </p>
+          <ul
+            v-if="errors"
+            class="error-messages"
+          >
+            <li
+              v-for="(v, k) in errors"
+              :key="k"
+            >
+              {{ k }} {{ v | error }}
+            </li>
+          </ul>
+          <form @submit.prevent="onSubmit(email, password)">
+            <fieldset class="form-group">
+              <input
+                v-model="email"
+                class="form-control form-control-lg"
+                placeholder="Email"
+                type="text"
+              >
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                v-model="password"
+                class="form-control form-control-lg"
+                placeholder="Password"
+                type="password"
+              >
+            </fieldset>
+            <button class="btn btn-lg btn-primary pull-xs-right">
+              Sign in
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
