1e22df52a2d71608a19b01bba8b2b4e6102d9c9e
landeaux <pluckedowl@gmail.com>
2019-11-23 11:06:03 -0800

    add profile view

---
+++ src/views/Profile.vue
@@ -0,0 +1,56 @@
+<script>
+import { mapGetters } from 'vuex'
+import {
+  FETCH_PROFILE
+} from '@/store/actions.type'
+
+export default {
+  name: 'RwvProfile',
+  computed: {
+    ...mapGetters(['currentUser', 'profile', 'isAuthenticated'])
+  },
+  watch: {
+    $route (to) {
+      this.$store.dispatch(FETCH_PROFILE, to.params)
+    }
+  },
+  mounted () {
+    this.$store.dispatch(FETCH_PROFILE, this.$route.params)
+  },
+  methods: {
+    isCurrentUser () {
+      if (this.currentUser.username && this.profile.username) {
+        return this.currentUser.username === this.profile.username
+      }
+      return false
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="profile-page">
+    <div class="user-info">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-md-10 offset-md-1">
+            <img
+              :src="profile.image"
+              class="user-img"
+            >
+            <h4>{{ profile.username }}</h4>
+            <p>{{ profile.bio }}</p>
+            <div v-if="isCurrentUser()">
+              <router-link
+                class="btn btn-sm btn-outline-secondary action-btn"
+                :to="{ name: 'settings' }"
+              >
+                <i class="ion-gear-a"></i> Edit Profile Settings
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
