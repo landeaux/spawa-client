f8ed78e6cca63f9f973969998a3d860b22934394
landeaux <pluckedowl@gmail.com>
2019-11-23 10:57:09 -0800

    import header component and use in place of nav container

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">
-        Home
-      </router-link> |
-      <router-link to="/about">
-        About
-      </router-link> |
-      <router-link to="/login">
-        Login
-      </router-link> |
-      <router-link to="/register">
-        Register
-      </router-link>
-    </div>
+    <RwvHeader />
     <router-view />
   </div>
 </template>
+
+<script>
+import RwvHeader from '@/components/TheHeader'
+
+export default {
+  name: 'App',
+  components: {
+    RwvHeader
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
