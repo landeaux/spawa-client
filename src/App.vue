d5ed0fa802fb584284b09ef6a070e1c88a76500d
landeaux <pluckedowl@gmail.com>
2019-11-23 12:01:17 -0800

    import and use Footer component; update global app styling

--- src/App.vue
+++ src/App.vue
@@ -2,44 +2,36 @@
   <div id="app">
     <RwvHeader />
     <router-view />
+    <RwvFooter />
   </div>
 </template>
 
 <script>
 import RwvHeader from '@/components/TheHeader'
+import RwvFooter from '@/components/TheFooter'
 
 export default {
   name: 'App',
   components: {
-    RwvHeader
+    RwvHeader,
+    RwvFooter
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="sass">
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
 
-#nav {
-  padding: 30px;
+a
+  color: #039
+  text-decoration: none
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    text-decoration: none;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-
-    &:hover {
-      color: #42b983;
-    }
-  }
-}
+  &:hover
+    color: #069
+    text-decoration: none
 </style>
