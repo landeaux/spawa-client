a8182a97475bde38f5f72fc0815bc34b7ee200ee
Christopher <chrisjmollise@gmail.com>
2020-01-25 18:16:30 -0800

    Added the four dashboard components

---
+++ src/components/AdminDashboard.vue
@@ -0,0 +1,21 @@
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'AdminDashboard',
+  computed: {
+    ...mapGetters(['currentUser']),
+  },
+};
+</script>
+
+<template>
+  <div class="greeting">
+    <h1>Admin Dashboard</h1>
+    <p>Welcome back, {{ currentUser.username }}!</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
