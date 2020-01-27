0a1ae630c5b8711a419ed5afda22a2232bd23a55
landeaux <pluckedowl@gmail.com>
2020-01-27 09:35:16 -0800

    Fix linter errors; Refactor for readability

--- src/assets/calendlyWidget.js
+++ src/assets/calendlyWidget.js
@@ -1,24 +1,34 @@
-/* eslint-disable */
 /*
-code is extracted from Calendly's embed script : https://assets.calendly.com/assets/external/widget.js
-only the createInlineWidgets() function is taken and modified to work on a passed in DOM node, rather than looking for
-a CSS class in the DOM
+ * This code is inspired by the vue-calendly npm package (https://officert.github.io/vue-calendly/#/home), which was
+ * built by extracting code from Calendly's embed script (https://assets.calendly.com/assets/external/widget.js) and
+ * abstracting the logic into a class. This code has been refactored for readability and best practices, as well as
+ * rewritten to leverage some new features from the Calendly embed script not currently present (as of this writing) in
+ * the vue-calendly package, such as passing prefill values and utm information to the constructor.
 */
 class Calendly {
-  constructor(options = {}) {
+  constructor (options = {}) {
     this.options = options;
     this.parseOptions();
     this.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
   }
 
-  $_assign (e) {
+  _assign (target) {
     'use strict';
-    if (e == null) throw new TypeError('Cannot convert undefined or null to object');
-    for (var t = Object(e), n = 1; n < arguments.length; n++) {
-      var o = arguments[n];
-      if (o != null) { for (var i in o) Object.prototype.hasOwnProperty.call(o, i) && (t[i] = o[i]); }
+    if (target === null) {
+      throw new TypeError('Cannot convert undefined or null to object');
     }
-    return t;
+    let targetRef = Object(target);
+    for (let i = 1; i < arguments.length; i++) {
+      let source = arguments[i];
+      if (source !== null) {
+        for (let key in source) {
+          if (Object.prototype.hasOwnProperty.call(source, key)) {
+            targetRef[key] = source[key];
+          }
+        }
+      }
+    }
+    return targetRef;
   }
 
   /**
@@ -29,10 +39,12 @@ class Calendly {
    * @param keys Array The allowable keys
    * @returns {{}} New filtered object
    */
-  $_pick (theObject, keys) {
-    let i, field, length = keys.length, result = {};
+  _pick (theObject, keys) {
+    let length = keys.length;
+    let field;
+    let result = {};
     if (theObject) {
-      for (i = 0; i < length; i++) {
+      for (let i = 0; i < length; i++) {
         field = keys[i];
         if (theObject[field]) {
           result[field] = theObject[field];
@@ -48,7 +60,7 @@ class Calendly {
    * @param theObject The object to convert the keys on
    * @returns {{}} New object with converted keys
    */
-  $_snakeCaseKeys (theObject) {
+  _snakeCaseKeys (theObject) {
     let newKey, oldKey, result;
     result = {};
     for (oldKey in theObject) {
@@ -58,24 +70,16 @@ class Calendly {
     return result;
   }
 
-  $_stripQuery (url) {
+  _stripQuery (url) {
     return url.split('?')[0];
   }
-
-  // build () {
-  //   this.node = document.createElement('iframe');
-  //   this.node.src = this.getSource();
-  //   this.node.width = '100%';
-  //   this.node.height = '100%';
-  //   return this.node.frameBorder = '0';
-  // }
 
   build () {
     this.node = document.createElement('iframe');
     this.node.src = this.getSource();
     this.node.width = '100%';
     this.node.height = '100%';
-    this.node.frameBorder = '0';
+    this.node.style.border = '0';
 
     this.node.onload = (e) => {
       this.onIframeLoaded(e);
@@ -94,26 +98,11 @@ class Calendly {
     return spinner;
   }
 
-  // buildSpinner () {
-  //   var t;
-  //   t = document.createElement("div");
-  //   t.className = "spinner";
-  //   t.appendChild(this.buildBounce(1));
-  //   t.appendChild(this.buildBounce(2));
-  //   t.appendChild(this.buildBounce(3));
-  //   return t;
-  // }
-
   buildBounce (bounceNum) {
     let bounce = document.createElement('div');
     bounce.className = 'calendly-bounce' + bounceNum;
     return bounce;
   }
-
-  // buildBounce (t) {
-  //   var e;
-  //   return e = document.createElement("div"), e.className = "bounce" + t, e;
-  // }
 
   /**
    * Extracts the parameters from the query string in a given URL
@@ -142,51 +131,47 @@ class Calendly {
   }
 
   filterConsentParam (url) {
-    return url.replace(/consent_accept=1&?/g, "");
+    return url.replace(/consent_accept=1&?/g, '');
   }
 
   format () {
-    return this.isMobile ? this.formatMobile() : this.formatDesktop();
+    return this.isMobile
+      ? this.formatMobile()
+      : this.formatDesktop();
   }
 
   formatDesktop () {
     return this.inlineStyles
-      ? this.parent.setAttribute('style', 'position: relative;' + this.parent.getAttribute('style'))
+      ? this.parent.setAttribute('style',
+        'position: relative;' +
+        this.parent.getAttribute('style'))
       : void 0;
   }
 
   formatMobile () {
-    return this.inlineStyles
-      ? this.parent.setAttribute('style', 'position: relative; overflow-y: auto; -webkit-overflow-scrolling: touch;' + this.parent.getAttribute('style'))
-      : this.parent.className += ' calendly-mobile';
+    if (this.inlineStyles) {
+      this.parent.setAttribute('style',
+        `
+        position: relative;
+        overflow-y: auto;
+        -webkit-overflow-scrolling: touch;
+        ` + this.parent.getAttribute('style'));
+    } else {
+      this.parent.className += ' calendly-mobile';
+    }
   }
 
-  // formatMobile () {
-  //   return this.inlineStyles
-  //     ? this.parent.setAttribute('style', 'position: relative; overflow-y:auto; -webkit-overflow-scrolling: touch;' + this.parent.getAttribute('style'))
-  //     : this.parent.className += ' mobile';
-  // }
-
-  getBookingUtmParams () {
-    let url = this.getUrl();
-
-    return this.getUtmUrlParams(url);
-  }
-
-  getDivider () {
-    return -1 === this.getUrl().indexOf('?') ? '?' : '&';
-  }
-
+  /**
+   * Gets the host domain.
+   *
+   * Though the host URI in most cases will not contain any characters which
+   * need encoding, we must account for cases where the host URI contains a
+   * port number (e.g. localhost:8080).
+   *
+   * @returns {string} The host domain
+   */
   getDomain () {
-    return document.location.host;
-  }
-
-  // getDomain () {
-  //   return encodeURIComponent(document.location.host);
-  // }
-
-  getHostUtmParams () {
-    return this.getUtmUrlParams(window.location.href);
+    return encodeURIComponent(document.location.host);
   }
 
   /**
@@ -196,7 +181,7 @@ class Calendly {
     */
   getParams () {
     let allParams, paramKey, paramValue, paramStrings;
-    allParams = this.$_assign({
+    allParams = this._assign({
       embed_domain: this.getDomain(),
       embed_type: this.embedType,
     }, this.getUtmParamsFromHost(), this.getParamsFromUrl(), this.getParamsFromOptions());
@@ -208,15 +193,8 @@ class Calendly {
     return paramStrings.join('&');
   }
 
-  // getParams() {
-  //   var t, e, n, o;
-  //   e = "embed_domain=" + this.getDomain() + "&embed_type=" + this.embedType, n = this.getHostUtmParams();
-  //   for (t in n) o = n[t], t in this.getBookingUtmParams() || (e += "&" + t + "=" + o);
-  //   return e;
-  // }
-
   getParamsFromOptions () {
-    return this.$_assign({}, this.getPrefillParams(), this.getUtmParams());
+    return this._assign({}, this.getPrefillParams(), this.getUtmParams());
   }
 
   getParamsFromUrl () {
@@ -233,7 +211,7 @@ class Calendly {
     let prefillParams, customAnswers, answerKey, answerValue;
     if (!this.options.prefill) return null;
     let keys = ['name', 'firstName', 'lastName', 'email'];
-    prefillParams = this.$_snakeCaseKeys(this.$_pick(this.options.prefill, keys));
+    prefillParams = this._snakeCaseKeys(this._pick(this.options.prefill, keys));
     if (this.options.prefill.customAnswers) {
       customAnswers = this.options.prefill.customAnswers;
       for (answerKey in customAnswers) {
@@ -247,20 +225,8 @@ class Calendly {
   }
 
   getSource () {
-    let url = this.$_stripQuery(this.url) + '?' + this.getParams();
+    let url = this._stripQuery(this.url) + '?' + this.getParams();
     return this.filterConsentParam(url);
-  }
-
-  // getSource () {
-  //   let source;
-  //   let params = this.getParams();
-  //   let url = '' + this.getUrl() + this.getDivider() + params;
-  //   let src = this.filterConsentParam(url);
-  //   return src;
-  // }
-
-  getUrl () {
-    return null != this.url ? this.url : this.url = this.parent.getAttribute("data-url").split("#")[0];
   }
 
   getUrlFromParent () {
@@ -270,26 +236,14 @@ class Calendly {
   getUtmParams () {
     let e = ['utmCampaign', 'utmSource', 'utmMedium', 'utmContent', 'utmTerm'];
     return this.options.utm
-      ? this.$_snakeCaseKeys(this.$_pick(this.options.utm, e))
+      ? this._snakeCaseKeys(this._pick(this.options.utm, e))
       : null;
-  }
-
-  getUrlParams (search = '') {
-    search = search.split('?')[1];
-
-    if (!search) return {};
-
-    return JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
   }
 
   getUtmParamsFromHost () {
     let utmParams = ['utm_campaign', 'utm_source', 'utm_medium', 'utm_content', 'utm_term'];
     let queryStringParams = this.extractQueryStringParams(window.location.href);
-    return this.$_pick(queryStringParams, utmParams);
-  }
-
-  getUtmUrlParams (url) {
-    return this.getUrlParams(url);
+    return this._pick(queryStringParams, utmParams);
   }
 
   inject () {
@@ -297,12 +251,6 @@ class Calendly {
     this.parent.appendChild(this.buildSpinner());
     return this.parent.appendChild(this.node);
   }
-
-  // inject () {
-  //   this.parent.appendChild(this.buildSpinner());
-  //   this.parent.appendChild(this.node);
-  //   return this.format();
-  // }
 
   parseOptions () {
     try {
@@ -324,10 +272,10 @@ class Calendly {
 }
 
 export default {
-  widget(options = {}) {
+  widget (options = {}) {
     const calendly = new Calendly(options);
     calendly.build();
     calendly.inject();
     return calendly;
-  }
-}
+  },
+};
