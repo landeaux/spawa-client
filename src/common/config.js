f6d91eac400e30aeb0aaf66242d6845a26817246
landeaux <pluckedowl@gmail.com>
2020-02-20 21:13:16 -0800

    remove staging from API config since its not an actual NODE_ENV

--- src/common/config.js
+++ src/common/config.js
@@ -1,6 +1,5 @@
 export const API_URL = {
   production: 'https://boiling-falls-11748.herokuapp.com/api',
-  staging: 'https://boiling-falls-11748.herokuapp.com/api',
   development: 'http://localhost:5000/api',
   test: 'http://localhost:5000/api',
 }[process.env.NODE_ENV];
