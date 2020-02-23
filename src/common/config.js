d9d002ca4c782d319aa3e02e9db9582f41cebc1a
landeaux <pluckedowl@gmail.com>
2020-02-23 13:53:35 -0800

    Add .env file for testing mode

--- src/common/config.js
+++
@@ -1,7 +0,0 @@
-export const API_URL = {
-  production: 'https://boiling-falls-11748.herokuapp.com/api',
-  development: 'http://localhost:5000/api',
-  test: 'http://localhost:5000/api',
-}[process.env.NODE_ENV];
-
-export default API_URL;
