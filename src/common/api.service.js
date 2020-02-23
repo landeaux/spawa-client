bc29108863f529fbb5e17f96dec91a333b828a07
landeaux <pluckedowl@gmail.com>
2020-02-23 13:57:31 -0800

    Use process.env.API_URL instead of config.js var

--- src/common/api.service.js
+++ src/common/api.service.js
@@ -2,12 +2,11 @@ import Vue from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 import JwtService from '@/common/jwt.service';
-import { API_URL } from '@/common/config';
 
 const ApiService = {
   init () {
     Vue.use(VueAxios, axios);
-    Vue.axios.defaults.baseURL = API_URL;
+    Vue.axios.defaults.baseURL = process.env.API_URL;
   },
 
   setHeader () {
