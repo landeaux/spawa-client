4917cc38c2c61d867a364ffeb5c6c47b3a918855
landeaux <pluckedowl@gmail.com>
2020-02-29 15:27:39 -0800

    remove login & register buttons from Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,29 +20,5 @@ export default {
     <EvaluatorDashboard v-else-if="currentUser.role === 'evaluator'" />
     <ReviewerDashboard v-else-if="currentUser.role === 'reviewer'" />
     <FounderDashboard v-else-if="currentUser.role === 'founder'" />
-    <div v-else>
-      <router-link
-        class="nav-link"
-        :to="{ name: 'login' }"
-      >
-        <button
-          type="button"
-          class="btn btn-primary"
-        >
-          Login
-        </button>
-      </router-link>
-      <router-link
-        class="nav-link"
-        :to="{ name: 'register' }"
-      >
-        <button
-          type="button"
-          class="btn btn-primary"
-        >
-          Register
-        </button>
-      </router-link>
-    </div>
   </div>
 </template>
