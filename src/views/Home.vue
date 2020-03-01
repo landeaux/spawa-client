de8277274db137da16de328533598f6ef3765eb7
landeaux <pluckedowl@gmail.com>
2020-02-29 16:48:58 -0800

    Make dashboard component/loader render dynamically

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,18 +8,60 @@ export default {
     EvaluatorDashboard: () => import('@/components/EvaluatorDashboard'),
     FounderDashboard: () => import('@/components/FounderDashboard'),
     ReviewerDashboard: () => import('@/components/ReviewerDashboard'),
+    Loader: () => import('vue-spinner/src/PulseLoader.vue'),
   },
   computed: {
     ...mapGetters(['currentUser']),
+    dashboardComponent () {
+      const componentKey = this.currentUser && this.currentUser.role
+        ? this.currentUser.role
+        : 'loader';
+      // const componentKey = 'loader';
+      return {
+        admin: 'AdminDashboard',
+        evaluator: 'EvaluatorDashboard',
+        founder: 'FounderDashboard',
+        reviewer: 'ReviewerDashboard',
+        loader: 'Loader',
+      }[componentKey];
+    },
+    dashboardComponentProps () {
+      return this.currentUser && this.currentUser.role
+        ? {}
+        : {
+          color: 'blue',
+          size: '25px',
+        };
+    },
+    dashboardComponentClasses () {
+      return this.dashboardComponent === 'Loader'
+        ? 'loader'
+        : 'component';
+    },
   },
 };
 </script>
 
 <template>
   <div id="view">
-    <AdminDashboard v-if="currentUser.role === 'admin'" />
-    <EvaluatorDashboard v-else-if="currentUser.role === 'evaluator'" />
-    <ReviewerDashboard v-else-if="currentUser.role === 'reviewer'" />
-    <FounderDashboard v-else-if="currentUser.role === 'founder'" />
+    <component
+      :is="dashboardComponent"
+      v-bind="dashboardComponentProps"
+      :class="dashboardComponentClasses"
+    />
   </div>
 </template>
+
+<style scoped lang="sass">
+@import '../assets/sass/vars'
+#view
+  .loader
+    position: relative
+    height: 100%
+    display: flex
+    flex-direction: row
+    justify-content: center
+    align-items: center
+  .component
+    flex-basis: 100%
+</style>
