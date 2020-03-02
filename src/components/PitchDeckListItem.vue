dc1efbb521ee4f14003b458de90f982183c7ea35
landeaux <pluckedowl@gmail.com>
2020-03-01 19:05:03 -0800

    add initial unit test suite and PitchDeckListItem component

---
+++ src/components/PitchDeckListItem.vue
@@ -0,0 +1,22 @@
+<script>
+/**
+ * @displayName PitchDeckListItem.vue
+ */
+export default {
+  name: 'PitchDeckListItemVue',
+  components: {},
+  props: {},
+  data: () => ({}),
+  computed: {},
+  methods: {},
+};
+</script>
+
+<template>
+  <div>
+  </div>
+</template>
+
+<style scoped lang="sass">
+
+</style>
---
+++ src/components/__tests__/PitchDeckListItem.spec.js
@@ -0,0 +1,8 @@
+// import { shallowMount } from '@vue/test-utils';
+// import PitchDeckListItem from '@/components/PitchDeckListItem';
+
+describe('PitchDeckListItem.vue', function () {
+  it('sanity check', function () {
+    expect(true).toBe(false);
+  });
+});
