b060010e3573ca94d81d28448dca0e677aaf8dde
landeaux <pluckedowl@gmail.com>
2020-03-02 21:03:11 -0800

    implement initial PitchDeskList.vue that passes all tests

---
+++ src/components/PitchDeckList.vue
@@ -0,0 +1,37 @@
+<script>
+import PitchDeckListItem from '@/components/PitchDeckListItem';
+
+/**
+ * PitchDeckList
+ */
+export default {
+  name: 'PitchDeckList',
+  components: {
+    PitchDeckListItem, // : () => import('@/components/PitchDeckListItem'),
+  },
+  props: {
+    items: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <PitchDeckListItem
+      v-for="(item, index) in items"
+      :key="index"
+      :business-name="item.businessName"
+      :date-submitted="item.dateSubmitted"
+      :review-count="item.reviewCount"
+      :user-has-reviewed="item.userHasReviewed"
+    />
+  </div>
+</template>
+
+<style scoped lang="sass">
+
+</style>
