2b7e20f49230ce3d9c17bfffa5c21126beb08320
Christopher <chrisjmollise@gmail.com>
2020-03-02 19:41:14 -0800

    Adjusted the Founder Dashboard to match design document. Added a big button component for reuse in later dashboard designs.

---
+++ src/components/BigButtonComponent.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: 'BigButtonComponent',
+  props: {
+    title: {
+      default: '',
+      required: true,
+      type: String,
+    },
+    subtitle: {
+      default: '',
+      required: false,
+      type: String,
+    },
+    num: {
+      default: 0,
+      required: false,
+      type: Number,
+    },
+  },
+};
+</script>
+
+<template>
+  <button
+    type="button"
+    class="big-button btn btn-primary"
+  >
+    <b>{{ title }}</b><br>{{ subtitle }} {{ num }}
+  </button>
+</template>
+
+<style scoped>
+  .big-button {
+    font-size: 120%;
+    width: 15rem;
+    height: 15rem;
+    border-radius: 16px;
+  }
+</style>
