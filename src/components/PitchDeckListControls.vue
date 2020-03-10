6386a49c7155a81fe721497b9f03d0517fc56ded
landeaux <pluckedowl@gmail.com>
2020-03-09 19:11:28 -0700

    Add pitchDeckCount prop and render it

--- src/components/PitchDeckListControls.vue
+++ src/components/PitchDeckListControls.vue
@@ -4,16 +4,26 @@
  */
 export default {
   name: 'PitchDeckListControls',
-  props: {},
+  props: {
+    pitchDeckCount: {
+      default: 0,
+      required: false,
+      type: Number,
+      validator: Number.isInteger,
+    },
+  },
 };
 </script>
 
 <template>
   <div class="controls-container border">
+    <div class="pitch-deck-count">
+      {{ pitchDeckCount }} Total Pitch Decks
+    </div>
     <div class="filter-wrapper">
       <b-dropdown
         id="dropdown-filter"
-        text="Filter"
+        text="Filter By"
         class="m-md-2"
       >
         <b-dropdown-item>First Filter</b-dropdown-item>
@@ -23,7 +33,7 @@ export default {
     <div class="sort-wrapper">
       <b-dropdown
         id="dropdown-sort"
-        text="Sort"
+        text="Sort By"
         class="m-md-2"
       >
         <b-dropdown-item>First Sort</b-dropdown-item>
