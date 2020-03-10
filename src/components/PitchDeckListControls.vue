d384c6c7508a4a666293086db317e35c6c2aff47
landeaux <pluckedowl@gmail.com>
2020-03-09 18:19:07 -0700

    Add styling, dropdown labels

--- src/components/PitchDeckListControls.vue
+++ src/components/PitchDeckListControls.vue
@@ -9,24 +9,34 @@ export default {
 </script>
 
 <template>
-  <div>
-    <b-dropdown
-      id="dropdown-filter"
-      text="Dropdown Button"
-      class="m-md-2"
-    >
-      <b-dropdown-item>First Filter</b-dropdown-item>
-    </b-dropdown>
-    <b-dropdown
-      id="dropdown-sort"
-      text="Dropdown Button"
-      class="m-md-2"
-    >
-      <b-dropdown-item>First Sort</b-dropdown-item>
-    </b-dropdown>
+  <div class="controls-container border">
+    <div class="filter-wrapper">
+      <b-dropdown
+        id="dropdown-filter"
+        text="Filter"
+        class="m-md-2"
+      >
+        <b-dropdown-item>First Filter</b-dropdown-item>
+      </b-dropdown>
+    </div>
+
+    <div class="sort-wrapper">
+      <b-dropdown
+        id="dropdown-sort"
+        text="Sort"
+        class="m-md-2"
+      >
+        <b-dropdown-item>First Sort</b-dropdown-item>
+      </b-dropdown>
+    </div>
   </div>
 </template>
 
 <style scoped lang="sass">
-
+.controls-container
+  width: 100%
+  display: flex
+  flex-direction: row
+  justify-content: space-between
+  background-color: #e4f7ff
 </style>
