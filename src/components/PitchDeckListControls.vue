7aee03211cd726788d5b11b2961c1d8cdf6356f5
landeaux <pluckedowl@gmail.com>
2020-03-09 17:43:51 -0700

    Create initial PitchDeckListControls component

---
+++ src/components/PitchDeckListControls.vue
@@ -0,0 +1,32 @@
+<script>
+/**
+ * PitchDeckListControls
+ */
+export default {
+  name: 'PitchDeckListControls',
+  props: {},
+};
+</script>
+
+<template>
+  <div>
+    <b-dropdown
+      id="dropdown-filter"
+      text="Dropdown Button"
+      class="m-md-2"
+    >
+      <b-dropdown-item>First Filter</b-dropdown-item>
+    </b-dropdown>
+    <b-dropdown
+      id="dropdown-sort"
+      text="Dropdown Button"
+      class="m-md-2"
+    >
+      <b-dropdown-item>First Sort</b-dropdown-item>
+    </b-dropdown>
+  </div>
+</template>
+
+<style scoped lang="sass">
+
+</style>
