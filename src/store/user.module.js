95dd8098cb1dee190fb650bd2fb106a3e5e7da7f
landeaux <pluckedowl@gmail.com>
2020-03-27 09:42:50 -0700

    Implement createUser action

--- src/store/user.module.js
+++ src/store/user.module.js
@@ -1,3 +1,11 @@
+import ApiService from '@/common/api.service';
+import {
+  CREATE_USER,
+} from '@/store/actions.type';
+import {
+  SET_ERROR,
+} from '@/store/mutations.type';
+
 const state = {
 
 };
@@ -7,7 +15,14 @@ const getters = {
 };
 
 const actions = {
-
+  async [CREATE_USER] (context, user) {
+    try {
+      const { data } = await ApiService.post('user', { user });
+      return data;
+    } catch ({ response }) {
+      context.commit(SET_ERROR, response.data.errors);
+    }
+  },
 };
 
 const mutations = {
