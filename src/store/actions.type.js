1745fe7e50ce14b8d9f18fa5d909dc51694ebf14
landeaux <pluckedowl@gmail.com>
2020-03-27 13:20:10 -0700

    Add SUSPEND_USER action type

--- src/store/actions.type.js
+++ src/store/actions.type.js
@@ -7,3 +7,4 @@ export const FETCH_PROFILE = 'fetchProfile';
 export const FETCH_PROFILE_BY_USERNAME = 'fetchProfileByUsername';
 export const CREATE_USER = 'createUser';
 export const FETCH_USERS = 'fetchUsers';
+export const SUSPEND_USER = 'suspendUser';
