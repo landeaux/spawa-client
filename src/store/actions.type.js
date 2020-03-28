1a7d491182a92554555aa229e6142bd78d8280c7
landeaux <pluckedowl@gmail.com>
2020-03-27 18:39:13 -0700

    Add ACTIVATE_USER action type

--- src/store/actions.type.js
+++ src/store/actions.type.js
@@ -8,3 +8,4 @@ export const FETCH_PROFILE_BY_USERNAME = 'fetchProfileByUsername';
 export const CREATE_USER = 'createUser';
 export const FETCH_USERS = 'fetchUsers';
 export const SUSPEND_USER = 'suspendUser';
+export const ACTIVATE_USER = 'activateUser';
