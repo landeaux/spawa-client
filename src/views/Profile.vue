ff7b58f6e17936a40e5a68e6c55b52284fdeecc2
landeaux <pluckedowl@gmail.com>
2020-03-28 18:51:59 -0700

    Update Profile view to grab HubSpot contact info

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -7,12 +7,35 @@ import {
 
 export default {
   name: 'TheProfile',
+  components: {
+    PulseLoader: () => import('vue-spinner/src/PulseLoader.vue'),
+  },
+  data: () => ({
+    state: 'INIT',
+  }),
   computed: {
     ...mapGetters([
       'currentUser',
       'profile',
       'isAuthenticated',
+      'profileErrors',
     ]),
+    currentUserIsAdmin () {
+      return this.currentUser
+        ? this.currentUser.role === 'admin'
+        : false;
+    },
+    showLoader () {
+      return this.state === 'INIT';
+    },
+    showError () {
+      return this.state === 'FETCH_ERROR';
+    },
+    company () {
+      return this.profile.pitchDeck
+        ? this.profile.pitchDeck.company
+        : '';
+    },
   },
   watch: {
     $route (to) {
@@ -21,9 +44,15 @@ export default {
     currentUser () {
       this.fetchProfile(this.$route.params);
     },
+    profileErrors () {
+      if (this.profileErrors.length > 0) this.state = 'FETCH_ERROR';
+    },
+    profile () {
+      if (this.profile) this.state = 'FETCH_SUCCESS';
+    },
   },
-  mounted () {
-    this.fetchProfile(this.$route.params);
+  async mounted () {
+    await this.fetchProfile(this.$route.params);
   },
   methods: {
     async fetchProfile (params) {
@@ -37,8 +66,8 @@ export default {
       }
     },
     isCurrentUser () {
-      if (this.currentUser.username && this.profile.username) {
-        return this.currentUser.username === this.profile.username;
+      if (this.currentUser.username) {
+        return this.currentUser.username === this.$route.params.username;
       }
       return false;
     },
@@ -47,26 +76,79 @@ export default {
 </script>
 
 <template>
-  <div class="profile-page">
-    <div class="user-info">
+  <div
+    id="view"
+    class="profile-page"
+  >
+    <PulseLoader
+      v-if="showLoader"
+      class="loader"
+      color="blue"
+      size="25px"
+    />
+    <div
+      v-else-if="showError"
+    >
+      There was an error fetching the profile.
+    </div>
+    <div
+      v-else
+      class="user-info"
+    >
       <div class="container">
-        <div class="row">
-          <div class="col-xs-12 col-md-10 offset-md-1">
-            <img
-              :src="profile.image"
-              class="user-img"
-            >
-            <h4>{{ profile.username }}</h4>
-            <p>{{ profile.bio }}</p>
+        <div>
+          <div class="header">
+            <h1>{{ company }}</h1>
             <div v-if="isCurrentUser()">
               <router-link
                 class="btn btn-sm btn-outline-secondary action-btn"
                 :to="{ name: 'settings' }"
               >
-                <i class="ion-gear-a"></i> Edit Profile Settings
+                <i class="ion-gear-a"></i> Edit User Settings
               </router-link>
             </div>
+            <div v-else-if="currentUserIsAdmin">
+              <a
+                class="btn btn-sm btn-outline-secondary action-btn"
+                :href="profile.profileUrl"
+              >
+                HubSpot Profile
+              </a>
+            </div>
           </div>
+        </div>
+        <div>
+          <h2>Eapp</h2>
+          <ul>
+            <li
+              v-for="(value, key) in profile.eapp"
+              :key="key"
+            >
+              <strong>{{ key }}:</strong> {{ value }}
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2>Pitch Quiz</h2>
+          <ul>
+            <li
+              v-for="(value, key) in profile.pitchQuiz"
+              :key="key"
+            >
+              <strong>{{ key }}:</strong> {{ value }}
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2>Pitch Deck</h2>
+          <ul>
+            <li
+              v-for="(value, key) in profile.pitchDeck"
+              :key="key"
+            >
+              <strong>{{ key }}:</strong> {{ value }}
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -74,14 +156,12 @@ export default {
 </template>
 
 <style scoped lang="sass">
-  .profile-page .user-info
-    text-align: center
-    background: #f3f3f3
-    padding: 2rem 0 1rem
-
-  .profile-page .user-info .user-img
-    width: 100px
-    height: 100px
-    border-radius: 100px
-    margin-bottom: 1rem
+  #view
+    text-align: left
+    .header
+      text-align: center
+      background: #f3f3f3
+      padding: 2rem 0 1rem
+    li
+      list-style: none
 </style>
