ddc59d21c8f05086cf365390967724094f2cf79a
landeaux <pluckedowl@gmail.com>
2020-04-09 17:45:51 -0700

    Update styling for SubmitPitchDeck view

--- src/views/SubmitPitchDeckV2.vue
+++ src/views/SubmitPitchDeckV2.vue
@@ -11,6 +11,9 @@ const STATUS_FAILED = 3;
  */
 export default {
   name: 'SubmitPitchDeckV2',
+  components: {
+    PulseLoader: () => import('vue-spinner/src/PulseLoader.vue'),
+  },
   data () {
     return {
       currentStatus: null,
@@ -18,6 +21,11 @@ export default {
       uploadFieldName: 'pitchdeck',
       fileBlob: null,
       fileName: '',
+      instructions: 'Please submit your pitch deck using the form below.',
+      title: 'Submit Pitch Deck',
+      routerLinkTo: 'home',
+      successMessage: 'Thank you for submitting your pitch deck! Click "Next" to continue.',
+      inputHelp: 'We prefer PowerPoint, but you may submit .pdf',
     };
   },
   computed: {
@@ -68,9 +76,7 @@ export default {
      */
     onFileInputChange (evt) {
       const files = evt.target.files;
-
       if (!files.length) return;
-
       this.fileBlob = files[0];
       this.fileName = files[0].name;
     },
@@ -79,8 +85,6 @@ export default {
      * @param evt The event object
      */
     onFormSubmit (evt) {
-      console.log(evt);
-      console.log(evt.target);
       const formData = new FormData();
       if (!this.fileBlob || this.fileName === '') return;
       formData.append(this.uploadFieldName, this.fileBlob, this.fileName);
@@ -91,47 +95,75 @@ export default {
 </script>
 
 <template>
-  <div class="container">
-    <!--UPLOAD-->
-    <form
-      v-if="isInitial || isSaving"
-      enctype="multipart/form-data"
-      novalidate
-      @submit.prevent="onFormSubmit"
+  <div id="view">
+    <h1>{{ title }}</h1>
+    <p>{{ instructions }}</p>
+
+    <div
+      v-if="isInitial"
+      class="form"
     >
-      <h1>Upload Pitch Deck</h1>
-      <div class="dropbox">
-        <label
-          v-if="isInitial"
-          :for="uploadFieldName"
-        >
-          Select your file
-          <input
-            type="file"
-            :name="uploadFieldName"
-            :disabled="isSaving"
-            accept="application/pdf, application/vnd.openxmlformats-officedocument.presentationml.presentation"
-            class="input-file"
-            @change="onFileInputChange"
-          >
-        </label>
-        <p v-if="isSaving">
-          Uploading file...
-        </p>
-      </div>
-      <input
-        type="submit"
-        value="Submit"
+      <form
+        enctype="multipart/form-data"
+        novalidate
+        @submit.prevent="onFormSubmit"
       >
-    </form>
-    <!--SUCCESS-->
-    <div v-if="isSuccess">
-      <h2>Uploaded file successfully.</h2>
+        <fieldset>
+          <fieldset class="form-group">
+            <label
+              :for="uploadFieldName"
+              class="col-form-label col-form-label-lg"
+            >
+              Submit your pitch deck here
+            </label>
+            <p class="text-muted">
+              {{ inputHelp }}
+            </p>
+            <input
+              type="file"
+              :name="uploadFieldName"
+              accept="application/pdf, application/vnd.openxmlformats-officedocument.presentationml.presentation"
+              required
+              class="form-control form-control-lg"
+              @change="onFileInputChange"
+            >
+          </fieldset>
+          <input
+            type="submit"
+            value="Submit"
+            class="btn btn-lg btn-primary"
+          >
+        </fieldset>
+      </form>
     </div>
-    <!--FAILED-->
-    <div v-if="isFailed">
-      <h2>Uploaded failed.</h2>
+
+    <div
+      v-if="isSaving"
+      class="message"
+    >
       <p>
+        Uploading file...
+      </p>
+      <PulseLoader
+        class="loader"
+        color="blue"
+        size="25px"
+      />
+    </div>
+
+    <div
+      v-if="isSuccess"
+      class="message"
+    >
+      <p>{{ successMessage }}</p>
+    </div>
+
+    <div
+      v-if="isFailed"
+      class="message"
+    >
+      <p>
+        Uploaded failed.
         <a
           href="javascript:void(0)"
           @click="reset()"
@@ -141,14 +173,52 @@ export default {
       </p>
       <pre>{{ uploadError }}</pre>
     </div>
+
+    <router-link
+      v-if="isSuccess"
+      :to="{ name: routerLinkTo }"
+    >
+      <button
+        type="button"
+        class="btn btn-primary next-button"
+      >
+        Next
+      </button>
+    </router-link>
+    <button
+      v-else
+      type="button"
+      class="btn btn-primary next-button"
+      style="cursor: not-allowed"
+      disabled
+      title="Please fill out the form to continue."
+    >
+      Please Fill Out The Form To Continue
+    </button>
   </div>
 </template>
 
 <style lang="sass">
-  label
-    display: block
-    text-align: center
-    input
-      margin: auto
-      display: block
+  @import '../assets/sass/vars'
+  #view
+    display: flex
+    flex-direction: column
+    .message, .form
+      position: relative
+      height: 100%
+      display: flex
+      flex-direction: column
+      justify-content: center
+      align-items: center
+    .form
+      fieldset
+        &.form-group
+          label
+            font-weight: bold
+    h1
+      color: #039
+    p
+      color: #007bff
+    button
+     min-width: 30vw
 </style>
