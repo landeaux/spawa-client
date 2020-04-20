f857b0e73586c6105d369bdb690502ad6b676719
landeaux <pluckedowl@gmail.com>
2020-04-19 17:03:53 -0700

    Add strict mode

--- src/store/index.js
+++ src/store/index.js
@@ -9,6 +9,8 @@ import review from './modules/review.module';
 
 Vue.use(Vuex);
 
+const debug = process.env.NODE_ENV !== 'production';
+
 export default new Vuex.Store({
   modules: {
     auth,
@@ -17,4 +19,10 @@ export default new Vuex.Store({
     pitchDeck,
     review,
   },
+  /**
+   * In strict mode, whenever Vuex state is mutated outside of mutation
+   * handlers, an error will be thrown. This ensures that all state mutations
+   * can be explicitly tracked by debugging tools.
+   */
+  strict: debug,
 });
